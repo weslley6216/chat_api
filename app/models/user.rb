30485e68ee74f086f382b54b1857e540b83b0b9c
Weslley Campos <weslley.campos@rebase.com.br>
2025-03-13 18:47:35 -0300

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
