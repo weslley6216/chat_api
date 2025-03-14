6aab988be3451e2dc8a9f8134ffc1258f62f5a04
Weslley Campos <weslley.campos@rebase.com.br>
2025-03-14 14:23:50 -0300

    Fix tests user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ describe User, type: :model do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:username) }
-    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
 
     context 'when username or email is duplicated' do
       before { create(:user, username: 'john_doe', email: 'john@example.com') }
@@ -11,11 +11,13 @@ describe User, type: :model do
       it 'is invalid with a duplicate username' do
         user = FactoryBot.build(:user, username: 'john_doe', email: 'another@example.com')
         expect(user).not_to be_valid
+        expect(user.errors[:username]).to include("has already been taken")
       end
 
       it 'is invalid with a duplicate email' do
         user = FactoryBot.build(:user, username: 'another_user', email: 'john@example.com')
         expect(user).not_to be_valid
+        expect(user.errors[:email]).to include("has already been taken")
       end
     end
   end
