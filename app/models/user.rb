5e7e0c2a909ab60f25994b1c25c4f64e50d77fe8
Weslley Campos <weslley.campos@rebase.com.br>
2025-03-14 21:38:46 -0300

    Add password attribute to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ describe User, type: :model do
   context 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
     it { should validate_uniqueness_of(:username) }
     it { should validate_uniqueness_of(:email).case_insensitive }
 
