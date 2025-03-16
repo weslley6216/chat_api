37cc85ca7efd7e6fd45800d72d357bd2abb482ef
Weslley Campos <weslley.campos@rebase.com.br>
2025-03-16 20:54:54 -0300

    Add user serializer and return user to auhorization method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
 
   has_many :conversations_as_user_a, class_name: 'Conversation', foreign_key: 'user_a_id'
   has_many :conversations_as_user_b, class_name: 'Conversation', foreign_key: 'user_b_id'
